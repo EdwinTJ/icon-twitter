9d1409ee9e8cb787bb2cf379c9bb984249fae2dc
EdwinTJ <edwin16x@gmail.com>
2023-04-04 13:00:06 -0600

    Profile page with ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,24 +1,15 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
 import {
   createTRPCRouter,
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.firstName,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
-
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -26,32 +17,26 @@ const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
 });
-
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
       orderBy: [{ createdAt: "desc" }],
     });
-
     const users = (
       await clerkClient.users.getUserList({
         userId: posts.map((post) => post.authorId),
         limit: 100,
       })
     ).map(filterUserForClient);
-
     console.log(users);
-
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
-
       if (!author || !author.username)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Author for post not found",
         });
-
       return {
         post,
         author: {
@@ -61,7 +46,6 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
-
   create: privateProcedure
     .input(
       z.object({
@@ -70,17 +54,14 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
-
       const { success } = await ratelimit.limit(authorId);
       if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-
       const post = await ctx.prisma.post.create({
         data: {
           authorId,
           content: input.content,
         },
       });
-
       return post;
     }),
 });
